f77eda3be8cbbdd8957c34fd96bf9ddc889eac7e
E2Funny14 <eianjennings@gmail.com>
2024-09-08 23:36:02 -0400

    modal opened and closed

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,21 @@ const initialCards = [
   {name: "A very long bridge, over the forest and through the trees", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",},
 
   {name: "Tunnel with morning light", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",},
-  
+
   {name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",},
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const editModal = document.querySelector("#edit-modal");
+const closeProfileModal = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+};
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+};
+
+profileEditButton.addEventListener("click", openModal);
+closeProfileModal.addEventListener("click", closeModal);
+
